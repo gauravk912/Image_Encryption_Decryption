0b2a62afa365e88b02d5425bf6a83d53cf11756a
Janhavikom <84494006+Janhavikom@users.noreply.github.com>
2023-02-12 20:16:37 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,9 +1,9 @@
-
 from PIL import Image
 import numpy,random,sys
 import tkinter as tk
 from tkinter import filedialog
-
+print ('-- Image Encryption and Decryption --')
+print ('*************************************')
 file1 = filedialog.askopenfile(mode='r', filetype=[('jpg file', '*.jpg')])
 file_name = file1.name
 img=Image.open(file_name)
@@ -11,18 +11,18 @@ img=Image.open(file_name)
 arr=numpy.asarray(img)
 arr1=arr.copy()
 x,y,z=arr.shape
-n=y-(random.randint(y//4,y//2))
-n1=x-(random.randint(x//4,x//2))
+n=y-(random.randint(y//40,y//20))
+n1=x-(random.randint(x//40,x//20))
 m,m1=random.randint(1,1000),random.randint(1,1000)
 b,c=random.randint(1,1000),random.randint(1,1000)
-it=random.randint(50,2000)
+it=random.randint(50,200)
 def flip():
     global x,b,n1
-    for i in range(b,x-1,n1*200):
+    for i in range(b,x-1,n1*200000):
         arr1[i:i+n1]=numpy.fliplr(arr1[i:i+n1])
 def yflip():
     global y,n,b
-    for i in range(b,y-1,n*200):
+    for i in range(b,y-1,n*200000):
         arr1[:,i:i+n]=numpy.fliplr(arr1[:,i:i+n])
 print('')
 for i in range(it):#encryption
@@ -30,12 +30,15 @@ for i in range(it):#encryption
     c+=m
     yflip()
     b+=m1
-    print ('\r{}done'.format((i/it)*100),end='',flush=True)
+
+    print ('\r{}'.format((i/it)*100),end='',flush=True)
     sys.stdout.flush()
 print('')
 #text encryption
 li= [n,n1,m,m1,b,c,it]
-print('your key',' '.join(str(e) for e in li))
+print('Your key : ',' '.join(str(e) for e in li))
+print ('*************************************')
+
 
 enimg = Image.fromarray(numpy.uint8(arr1)).convert('RGB')
 enimg.show()
